9122464b69c76bed4fe489cf61bd01049bb83b38
Kosisochukwu Afoenyi <kosisochukwu.afoenyi@platoanalytics.com>
2023-03-05 20:39:32 +0100

    Install nextjs/reactjs typescript tailwindCSS and storybook

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,6 @@
+
+function Button({ children, pageProps }: any) {
+    return <button  {...pageProps}>{ children }</button>;
+}
+
+export default Button;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
+import Button from "../components/Button";
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,6 +17,8 @@ export default function Home() {
       </Head>
       <main className={styles.main}>
         <div className={styles.description}>
+            <h1 className='text-3xl font-bold underline'>Tailwind CSS</h1>
+            <Button>Button</Button>
           <p>
             Get started by editing&nbsp;
             <code className={styles.code}>src/pages/index.tsx</code>
